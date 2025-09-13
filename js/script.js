d42196e73345b9c82446143ec5861f917b85233a
Jack Webster <jack.ryan.webster@gmail.com>
2025-09-13 00:52:18 -0600

    Darken each panel on mouseover
    One of the project bonus challenges requires that each panel becomes
    progressively darker on each mouseover. This commit creates an event
    listener and a "darken" function to do so.

--- js/script.js
+++ js/script.js
@@ -7,9 +7,27 @@ for (let i = 0; i < squaresPerSide ** 2; i++)
   newPanel.setAttribute
   (
     "style", 
-    `width: ${container.offsetWidth / squaresPerSide}px; `
-    + `height: ${container.offsetHeight / squaresPerSide}px;`
+    `width: ${container.clientWidth / squaresPerSide}px; `
+    + `height: ${container.clientHeight / squaresPerSide}px;`
   );
   newPanel.classList.add("panel");
+  newPanel.addEventListener("mouseover", darken);
   container.appendChild(newPanel);
+}
+
+function darken(event) {
+  const panel = event.target;
+
+  const oldRgba = (
+    window
+    .getComputedStyle(panel)
+    .getPropertyValue("background-color")
+  );
+  const oldAlpha = parseFloat(oldRgba.slice(5,-1).split(", ")[3]);
+
+  const steps = 10;
+  const newAlpha = Math.min(oldAlpha + 1 / steps, 1.0);
+  const newRgba = oldRgba.replace(/[^,)\s]+(?=\s*\))/, newAlpha);
+  panel.style.backgroundColor = newRgba;
+  return
 }
