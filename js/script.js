a7f5ac1716aa86388df222140d49c2485ff7ff98
Jack Webster <jack.ryan.webster@gmail.com>
2025-09-13 02:13:42 -0600

    Add button to reset grid
    The last piece of the project is to add a button to reset the grid. The
    user is prompted for the number of panels per side, old panels are
    deleted, and new panels are added. This commit adds that functionality.

--- js/script.js
+++ js/script.js
@@ -1,27 +1,66 @@
 const container = document.querySelector("#container");
 
-const squaresPerSide = 16;
-for (let i = 0; i < squaresPerSide ** 2; i++)
-{
-  let newPanel = document.createElement("div");
-  newPanel.setAttribute
-  (
-    "style", 
-    `width: ${container.clientWidth / squaresPerSide}px; `
-    + `height: ${container.clientHeight / squaresPerSide}px;`
-    + `background-color: ${chooseColor()};`
+const button = document.querySelector("button");
+button.addEventListener("click", resetGrid);
+
+function getNumPanels(numOldPanels) {
+  const defaultPanels = 16;
+  let panelsPerSide = parseInt(
+    prompt("How many panels per side?", defaultPanels)
   );
-  newPanel.classList.add("panel");
-  newPanel.addEventListener("mouseover", darken);
-  container.appendChild(newPanel);
+
+  if (isNaN(panelsPerSide)) {
+    return numOldPanels;
+  }
+
+  const maxPanels = 100;
+  return Math.min(panelsPerSide, maxPanels);
+}
+
+function deletePanels(oldPanels) {
+  for (let panel of oldPanels) {
+    panel.remove();
+  }
+}
+
+function createPanels(container, panelsPerSide) {
+  for (let i = 0; i < panelsPerSide ** 2; i++) {
+    let newPanel = document.createElement("div");
+    newPanel.setAttribute(
+      "style",
+      `width: ${container.clientWidth / panelsPerSide}px; `
+      + `height: ${container.clientHeight / panelsPerSide}px;`
+      + `background-color: ${chooseColor()};`
+    );
+    newPanel.classList.add("panel");
+    newPanel.addEventListener("mouseover", darken);
+    container.appendChild(newPanel);
+  }
+}
+
+function resetGrid() {
+  const oldPanels = container.querySelectorAll(":scope > div");
+  const numOldPanels = oldPanels.length;
+
+  const panelsPerSide = getNumPanels(numOldPanels);
+
+  if (panelsPerSide === numOldPanels) {
+    return;
+  }
+
+  deletePanels(oldPanels);
+  createPanels(container, panelsPerSide);
+  return;
 }
 
 function chooseColor() {
   maxValue = 256;
   color = {};
+
   for (let val of ["r", "g", "b"]) {
     color[val] = Math.floor(Math.random() * maxValue);
   }
+
   return `rgba(${color.r}, ${color.g}, ${color.b}, 0.0)`
 }
 
@@ -31,7 +70,6 @@ function darken(event) {
   const oldRgba = (
     window
     .getComputedStyle(panel)
-    .getPropertyValue("background-color")
   );
   const oldAlpha = parseFloat(oldRgba.slice(5,-1).split(", ")[3]);
 
